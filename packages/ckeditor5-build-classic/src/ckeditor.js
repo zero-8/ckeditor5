7b5f19c2ae21950044afb52a36bd8c0772fa7681
zero-8 <jmangaluz@gmail.com>
2020-12-03 07:38:21 +0800

    revert ckeditor embed config

--- packages/ckeditor5-build-classic/src/ckeditor.js
+++ packages/ckeditor5-build-classic/src/ckeditor.js
@@ -919,115 +919,115 @@ ClassicEditor.defaultConfig = {
 			// 'horizontalLine',
 		]
 	},
-   	mediaEmbed: {
-        providers: [
-		    	{
-					name: 'dailymotion',
-					url: /^dailymotion\.com\/video\/(\w+)/,
-					html: match => {
-						const id = match[ 1 ];
+   	// mediaEmbed: {
+    //     providers: [
+		  //   	{
+				// 	name: 'dailymotion',
+				// 	url: /^dailymotion\.com\/video\/(\w+)/,
+				// 	html: match => {
+				// 		const id = match[ 1 ];
 
-						return (
-							'<div style="position: relative; padding-bottom: 100%; height: 0; ">' +
-								`<iframe src="https://www.dailymotion.com/embed/video/${ id }" ` +
-									'style="position: absolute; width: 100%; height: 100%; top: 0; left: 0;" ' +
-									'frameborder="0" width="480" height="270" allowfullscreen allow="autoplay">' +
-								'</iframe>' +
-							'</div>'
-						);
-					}
-				},
+				// 		return (
+				// 			'<div style="position: relative; padding-bottom: 100%; height: 0; ">' +
+				// 				`<iframe src="https://www.dailymotion.com/embed/video/${ id }" ` +
+				// 					'style="position: absolute; width: 100%; height: 100%; top: 0; left: 0;" ' +
+				// 					'frameborder="0" width="480" height="270" allowfullscreen allow="autoplay">' +
+				// 				'</iframe>' +
+				// 			'</div>'
+				// 		);
+				// 	}
+				// },
 
-				{
-					name: 'spotify',
-					url: [
-						/^open\.spotify\.com\/(artist\/\w+)/,
-						/^open\.spotify\.com\/(album\/\w+)/,
-						/^open\.spotify\.com\/(track\/\w+)/
-					],
-					html: match => {
-						const id = match[ 1 ];
+				// {
+				// 	name: 'spotify',
+				// 	url: [
+				// 		/^open\.spotify\.com\/(artist\/\w+)/,
+				// 		/^open\.spotify\.com\/(album\/\w+)/,
+				// 		/^open\.spotify\.com\/(track\/\w+)/
+				// 	],
+				// 	html: match => {
+				// 		const id = match[ 1 ];
 
-						return (
-							'<div style="position: relative; padding-bottom: 100%; height: 0; padding-bottom: 126%;">' +
-								`<iframe src="https://open.spotify.com/embed/${ id }" ` +
-									'style="position: absolute; width: 100%; height: 100%; top: 0; left: 0;" ' +
-									'frameborder="0" allowtransparency="true" allow="encrypted-media">' +
-								'</iframe>' +
-							'</div>'
-						);
-					}
-				},
+				// 		return (
+				// 			'<div style="position: relative; padding-bottom: 100%; height: 0; padding-bottom: 126%;">' +
+				// 				`<iframe src="https://open.spotify.com/embed/${ id }" ` +
+				// 					'style="position: absolute; width: 100%; height: 100%; top: 0; left: 0;" ' +
+				// 					'frameborder="0" allowtransparency="true" allow="encrypted-media">' +
+				// 				'</iframe>' +
+				// 			'</div>'
+				// 		);
+				// 	}
+				// },
 
-				{
-					name: 'youtube',
-					url: [
-						/^(?:m\.)?youtube\.com\/watch\?v=([\w-]+)/,
-						/^(?:m\.)?youtube\.com\/v\/([\w-]+)/,
-						/^youtube\.com\/embed\/([\w-]+)/,
-						/^youtu\.be\/([\w-]+)/
-					],
-					html: match => {
-						const id = match[ 1 ];
+				// {
+				// 	name: 'youtube',
+				// 	url: [
+				// 		/^(?:m\.)?youtube\.com\/watch\?v=([\w-]+)/,
+				// 		/^(?:m\.)?youtube\.com\/v\/([\w-]+)/,
+				// 		/^youtube\.com\/embed\/([\w-]+)/,
+				// 		/^youtu\.be\/([\w-]+)/
+				// 	],
+				// 	html: match => {
+				// 		const id = match[ 1 ];
 
-						return (
-							'<div style="position: relative; padding-bottom: 100%; height: 0; padding-bottom: 56.2493%;">' +
-								`<iframe src="https://www.youtube.com/embed/${ id }" ` +
-									'style="position: absolute; width: 100%; height: 100%; top: 0; left: 0;" ' +
-									'frameborder="0" allow="autoplay; encrypted-media" allowfullscreen>' +
-								'</iframe>' +
-							'</div>'
-						);
-					}
-				},
+				// 		return (
+				// 			'<div style="position: relative; padding-bottom: 100%; height: 0; padding-bottom: 56.2493%;">' +
+				// 				`<iframe src="https://www.youtube.com/embed/${ id }" ` +
+				// 					'style="position: absolute; width: 100%; height: 100%; top: 0; left: 0;" ' +
+				// 					'frameborder="0" allow="autoplay; encrypted-media" allowfullscreen>' +
+				// 				'</iframe>' +
+				// 			'</div>'
+				// 		);
+				// 	}
+				// },
 
-				{
-					name: 'vimeo',
-					url: [
-						/^vimeo\.com\/(\d+)/,
-						/^vimeo\.com\/[^/]+\/[^/]+\/video\/(\d+)/,
-						/^vimeo\.com\/album\/[^/]+\/video\/(\d+)/,
-						/^vimeo\.com\/channels\/[^/]+\/(\d+)/,
-						/^vimeo\.com\/groups\/[^/]+\/videos\/(\d+)/,
-						/^vimeo\.com\/ondemand\/[^/]+\/(\d+)/,
-						/^player\.vimeo\.com\/video\/(\d+)/
-					],
-					html: match => {
-						const id = match[ 1 ];
+				// {
+				// 	name: 'vimeo',
+				// 	url: [
+				// 		/^vimeo\.com\/(\d+)/,
+				// 		/^vimeo\.com\/[^/]+\/[^/]+\/video\/(\d+)/,
+				// 		/^vimeo\.com\/album\/[^/]+\/video\/(\d+)/,
+				// 		/^vimeo\.com\/channels\/[^/]+\/(\d+)/,
+				// 		/^vimeo\.com\/groups\/[^/]+\/videos\/(\d+)/,
+				// 		/^vimeo\.com\/ondemand\/[^/]+\/(\d+)/,
+				// 		/^player\.vimeo\.com\/video\/(\d+)/
+				// 	],
+				// 	html: match => {
+				// 		const id = match[ 1 ];
 
-						return (
-							'<div style="position: relative; padding-bottom: 100%; height: 0; padding-bottom: 56.2493%;">' +
-								`<iframe src="https://player.vimeo.com/video/${ id }" ` +
-									'style="position: absolute; width: 100%; height: 100%; top: 0; left: 0;" ' +
-									'frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen>' +
-								'</iframe>' +
-							'</div>'
-						);
-					}
-				},
+				// 		return (
+				// 			'<div style="position: relative; padding-bottom: 100%; height: 0; padding-bottom: 56.2493%;">' +
+				// 				`<iframe src="https://player.vimeo.com/video/${ id }" ` +
+				// 					'style="position: absolute; width: 100%; height: 100%; top: 0; left: 0;" ' +
+				// 					'frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen>' +
+				// 				'</iframe>' +
+				// 			'</div>'
+				// 		);
+				// 	}
+				// },
 
-				{
-					name: 'instagram',
-					url: /^instagram\.com\/p\/(\w+)/
-				},
-				{
-					name: 'twitter',
-					url: /^twitter\.com/
-				},
-				{
-					name: 'googleMaps',
-					url: /^google\.com\/maps/
-				},
-				{
-					name: 'flickr',
-					url: /^flickr\.com/
-				},
-				{
-					name: 'facebook',
-					url: /^facebook\.com/
-				}
-        ]
-    },
+				// {
+				// 	name: 'instagram',
+				// 	url: /^instagram\.com\/p\/(\w+)/
+				// },
+				// {
+				// 	name: 'twitter',
+				// 	url: /^twitter\.com/
+				// },
+				// {
+				// 	name: 'googleMaps',
+				// 	url: /^google\.com\/maps/
+				// },
+				// {
+				// 	name: 'flickr',
+				// 	url: /^flickr\.com/
+				// },
+				// {
+				// 	name: 'facebook',
+				// 	url: /^facebook\.com/
+				// }
+    //     ]
+    // },
 	fontColor: {
         colors: [
 
